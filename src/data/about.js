d6abac149707a17372612b91e4274c042949b906
Sean W. Lawrence <slawrence18@student.alamo.edu>
2017-11-16 23:44:39 -0600

    About page (#5)
    * responsive styling

---
+++ src/data/about.js
@@ -0,0 +1,26 @@
+const ABOUT_DATA = {
+  description: [
+    {
+      id: 1,
+      text: `I practically live, breath and dream in code... seriously. I've been creating and geeking out over websites ever since I could remember and love it so much that I spend about 12 hours a day coding on my laptop, designing mockups and perfecting my craft.`
+    },
+    {
+      id: 2,
+      text: `Whether you're a business, non-profit, open source project or blogger, I'd love to learn more about your project and help make it happen for you - I'm even happy to offer free advice. I can't wait to work together.`
+    }
+  ],
+  socialLinks: [
+    {
+      title: 'LinkedIn profile',
+      profile_url: 'https://www.linkedin.com/in/sean-lawrence-21792799/',
+      logo_url: 'https://media.graphcms.com/JbsaJHq2ThiZOMRnsCq3'
+    },
+    {
+      title: 'GitHub profile',
+      profile_url: 'https://github.com/seanwLawrence',
+      logo_url: 'https://media.graphcms.com/NGiCrbkcSLeVOc8QfNqw'
+    },
+  ]
+}
+
+export default ABOUT_DATA
