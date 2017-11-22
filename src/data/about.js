5ef6a88545a3a6bc4a4a08eadb885fa40a06dc6f
seanwlawrence <sean.w.lawrence@gmail.com>
2017-11-21 18:45:04 -0600

    initial commit

--- src/data/about.js
+++ src/data/about.js
@@ -2,11 +2,11 @@ const ABOUT_DATA = {
   description: [
     {
       id: 1,
-      text: `I practically live, breath and dream in code... seriously. I've been creating and geeking out over websites ever since I could remember and love it so much that I spend about 12 hours a day coding on my laptop, designing mockups and perfecting my craft.`
+      text: `I practically live, breath and dream in code... seriously. I've been creating and geeking out over websites ever since I could remember and love it so much that I spend about 12 hours a day coding on my laptop, designing mockups and perfecting my craft. `
     },
     {
       id: 2,
-      text: `Whether you're a business, non-profit, open source project or blogger, I'd love to learn more about your project and help make it happen for you - I'm even happy to offer free advice. I can't wait to work together.`
+      text: `Whether you're a business, non-profit, open source project or blogger, I'd love to learn more about your project and help make it happen for you - I'm even happy to offer free advice. I also play guitar (blues mostly), powerlift (185 lb bench, 285 lb squat 235 lb deadlift), ride motorcycles, travel around the world (3+ countries and 20+ states in the US), and shoot photos/videos in my spare time. Send me a message on my home page if you'd like to chat about your project or geek out over any of the hobbies I just mentioned. Talk soon!`
     }
   ],
   socialLinks: [
