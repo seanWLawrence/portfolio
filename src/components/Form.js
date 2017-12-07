14df9c6f703d7eaae89165d40ae63f7f9ab3e4de
seanwlawrence <sean.w.lawrence@gmail.com>
2017-12-07 17:56:02 -0600

    submit button and stateful form demo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Form = () => (
-  <form id='contact_form' name='contact' action='https://seanlawrence.co/thank-you' method="post" data-netlify="true" data-netlify-honeypot="bot-field" >
-    <input type="hidden" name="contact" value="contact" />
-    <label>
-      <input type="text" name="name" placeholder="Name" required />
-    </label>
-    <label>
-      <input type="email" name="email" placeholder="Email" required />
-    </label>
-    <label>
-      <textarea name="message" placeholder="Message" required></textarea>
-    </label>
-    <button
-      className='button_primary'
-      type="submit"
-    >
-      Send
-    </button>
-  </form>
-)
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-export default Form
+export default class Contact extends Component {
+  state = {}
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Message sent"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+        <form
+          name="contact"
+          method="post"
+          action="/thank-you/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+          id='contact_form'
+        >
+          <input name="bot-field" hidden/>
+          <input 
+            type="text" 
+            name="name" 
+            placeholder='Name'
+            onChange={this.handleChange} />
+          <input 
+            type="email" 
+            name="email" 
+            placeholder='Email'
+            onChange={this.handleChange} />
+          <textarea 
+            name="message" 
+            placeholder='Message'
+            onChange={this.handleChange} />
+          <button
+            className='button_primary'
+            type="submit">
+              Send
+          </button>
+        </form>
+    );
+  }
+}
