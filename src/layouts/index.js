3a9581105d538db23eeff965cc700dd00b4b621d
seanwlawrence <sean.w.lawrence@gmail.com>
2017-12-25 20:00:09 -0600

    updated favicon and compressed profile image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Header from './header'
-import Helmet from 'gatsby-plugin-react-helmet'
+import Helmet from 'react-helmet'
 
 import './index.scss'
 import 'prismjs/themes/prism.css'
 
 const TemplateWrapper = ({ children }) => (
   <section id='site_wrapper'>
+    <Helmet>
+      <meta property="og:site_name" content="Sean Lawrence - Modern web developer" />
+    </Helmet>
     <Header />
     <main id='site_content'>
       {children()}
+      <img src={Favicon} />
     </main>
   </section >
 )
