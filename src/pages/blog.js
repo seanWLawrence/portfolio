18f574eeb4da5d65d5a649f781dc69a441068065
seanwlawrence <sean.w.lawrence@gmail.com>
2018-05-20 19:54:17 -0500

    Sort blog posts in descending order

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -26,10 +26,10 @@ export default ({ data }) => {
   let imagesAndPosts = [...imagesWithId, ...postsWithId]
 
   let sortedImagesAndPosts = imagesAndPosts.sort((a, b) => {
-    if (a.id < b.id) {
+    if (a.id > b.id) {
       return -1;
     }
-    if (a.id > b.id) {
+    if (a.id < b.id) {
       return 1;
     }
     return 0;
